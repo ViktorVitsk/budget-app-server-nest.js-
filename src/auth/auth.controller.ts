c8bd610d8d95aa3b861588bb6e08a37ea2ad8bf6
Vitsk <viktor.web.frontend@gmail.com>
2023-12-02 12:54:23 +0200

    feat: add jwt auth guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from '@src/auth/guards/local-auth.guard';
+import { JwtAuthGuard } from '@src/auth/guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +11,11 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
